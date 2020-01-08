ba3c1dfa1d3ba37a021c204ac0a517636f1c240b
misterhaan <misterhaan@track7.org>
2020-01-07 21:49:11 -0600

    update copyright year

--- Properties/AssemblyInfo.Common.cs
+++ Properties/AssemblyInfo.Common.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("the analog underground")]
 [assembly: AssemblyProduct("MythClient")]
-[assembly: AssemblyCopyright("© 2012 - 2019 the analog underground")]
+[assembly: AssemblyCopyright("© 2012 - 2020 the analog underground")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
